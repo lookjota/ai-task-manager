c41ed003db8d66e1532f580c6a968777004d0af4
lookjota <lookjota@gmail.com>
2025-10-27 17:41:06 -0300

    configuration vite.config.ts to fix error database turso

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  define: {
-    // Expose environment variables to the client
-    'process.env.TURSO_DATABASE_URL': JSON.stringify(process.env.TURSO_DATABASE_URL),
-    'process.env.TURSO_AUTH_TOKEN': JSON.stringify(process.env.TURSO_AUTH_TOKEN),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    define: {
+      // Expose environment variables to the client
+      'process.env.TURSO_DATABASE_URL': JSON.stringify(env.TURSO_DATABASE_URL),
+      'process.env.TURSO_AUTH_TOKEN': JSON.stringify(env.TURSO_AUTH_TOKEN),
+    },
+  };
 });
